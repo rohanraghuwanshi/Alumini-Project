6f8d3b38b1af6a1e5d172f0ecb2fd58bfe94c72b
Rohan Raghuvanshi <rohanraghuvanshi001@gmail.com>
2020-01-30 19:51:14 +0530

    Created the Profile and College-details model.

--- Alumini/profiles/admin.py
+++ Alumini/profiles/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Profile, CollegeDetails
+
+# Make admin classes here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'profession', 'phone']
+
+class CollegeDetailsAdmin(admin.ModelAdmin):
+    list_display = ['user', 'course_name', 'branch', 'batch']
+
+
+
 # Register your models here.
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(CollegeDetails, CollegeDetailsAdmin)
