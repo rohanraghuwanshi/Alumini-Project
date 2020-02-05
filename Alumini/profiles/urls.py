6b8f9cbb3ec0d6c9c7f94ca23cd9c81b3990e84c
Rohan Raghuvanshi <rohanraghuvanshi001@gmail.com>
2020-02-05 12:04:31 +0530

    Created the user registration.

--- Alumini/profiles/urls.py
+++ Alumini/profiles/urls.py
@@ -7,5 +7,6 @@ app_name = 'profiles'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path("registration/", UserRegistrationView.as_view(), name="registration"),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- Alumini/profiles/views.py
+++ Alumini/profiles/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
+
+from .forms import UserRegistrationForm
 
 # Create your views here.
 
+class UserRegistrationView(CreateView):
+    model = User
+    form_class = UserRegistrationForm
+
+    template_name = "registration/registration.html"
+
+    def form_valid(self, form):
+        if form.cleaned_data['password'] == form.cleaned_data['confirm_password']:
+            user = form.save(commit=False)
+            user.set_password(user.password)
+            user.save()
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password'],
+                                    )
+            # login(self.request, new_user)
+            return redirect('/register/add-profile-picture')
+        else:
+            return super().form_valid(form)
