83f6ae7038a3b0e903dc0547d299b674c793aec8
Rohan Raghuvanshi <rohanraghuvanshi001@gmail.com>
2020-02-18 14:51:36 +0530

    First_deployment

--- Alumini/settings.py
+++ Alumini/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tt*d1t@#(uzft&3k84-v3*&i=_s%jdgj5!a3503s__j^u*fzx='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
