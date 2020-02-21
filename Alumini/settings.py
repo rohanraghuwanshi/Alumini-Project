64b6461dd0a67c558f0b0897b5d31234d590a028
Rohan Raghuvanshi <rohanraghuvanshi001@gmail.com>
2020-02-22 03:48:32 +0530

    Changes

--- Alumini/settings.py
+++ Alumini/settings.py
@@ -121,9 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    # os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
