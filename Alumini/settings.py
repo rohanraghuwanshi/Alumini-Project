a11b2cc286e3d36e8940c3332eaf43bf4f74a08d
Rohan Raghuvanshi <rohanraghuvanshi001@gmail.com>
2020-02-22 03:52:17 +0530

    Changes

--- Alumini/settings.py
+++ Alumini/settings.py
@@ -123,7 +123,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
