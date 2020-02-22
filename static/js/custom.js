8cd55e3f614d7e4015d0d7742bfe36baf4190f12
Rohan Raghuvanshi <rohanraghuvanshi001@gmail.com>
2020-02-22 15:25:35 +0530

    Login frontend added

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,16 +1,3 @@
-var swiper = new Swiper('.swiper-container', {
-    cssMode: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination'
-    },
-    mousewheel: true,
-    keyboard: true,
-});
-
 const toggler = document.querySelector(".menu_toggler");
 const menu = document.querySelector(".menu");
 
