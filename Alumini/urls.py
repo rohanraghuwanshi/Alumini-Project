a05660319947dacecd84190474fd2532b2e91f26
Rohan Raghuvanshi <rohanraghuvanshi001@gmail.com>
2020-02-22 23:21:01 +0530

    Added Frontend to registration

--- Alumini/urls.py
+++ Alumini/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('profiles.urls')),
     path('', include('homeapp.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 if settings.DEBUG==True:
